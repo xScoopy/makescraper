5b19f411272fbd63aa5576850bf103c24f8252b5
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-02-26 00:52:46 -0600

    testing that the scraper can receive responses from the steam store

--- scrape.go
+++ scrape.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gocolly/colly"
 )
 
-type scrapeData struct {
+type gameInfo struct {
 	Name string
 	Price float32
 	ReleaseDate string
@@ -18,20 +19,28 @@ type scrapeData struct {
 func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
+	c.SetRequestTimeout(120 * time.Second)
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	// // On every a element which has href attribute call callback
+	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+    //             link := e.Attr("href")
 
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-	})
-
-	// Before making a request print "Visiting ..."
+	// 	// Print link
+    //             fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	// })
+	
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
+		fmt.Println("Visiting", r.URL)
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Got a response from", r.Request.URL)
+	})
+
+	c.OnError(func(r *colly.Response, e error) {
+		fmt.Println("Received error:", e)
+	})
+
+	c.Visit("https://store.steampowered.com/search/?filter=topsellers")
+
 }
